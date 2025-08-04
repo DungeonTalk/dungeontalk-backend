29a032302ee8a09225d656350092ee5f452395e7
codesche <codesche@gmail.com>
2025-08-04 14:14:49 +0900

    Initial commit

---
+++ src/main/java/org/com/dungeontalk/DungeontalkApplication.java
@@ -0,0 +1,13 @@
+package org.com.dungeontalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeontalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DungeontalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/com/dungeontalk/DungeontalkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.dungeontalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeontalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
