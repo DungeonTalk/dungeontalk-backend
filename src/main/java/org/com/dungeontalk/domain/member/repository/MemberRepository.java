219a9739f48cc37a9d63bcf4d101fb7ee77201ba
codesche <codesche@gmail.com>
2025-08-07 15:56:43 +0900

    feat(chat): 채팅방 내 메시지 페이징 조회 위하여 findByIdIn 추가

--- src/main/java/org/com/dungeontalk/domain/member/repository/MemberRepository.java
+++ src/main/java/org/com/dungeontalk/domain/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package org.com.dungeontalk.domain.member.repository;
 
+import java.util.List;
 import org.com.dungeontalk.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,7 +14,8 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     // 중복 방지용 : 아이디로 유저 객체 찾기
     Optional<Member> findByName(String name);
 
-
+    // 회원 닉네임 일괄 조회 (채팅방 내 메시지 페이징 조회)
+    List<Member> findByIdIn(List<String> ids);
 
 
 }
