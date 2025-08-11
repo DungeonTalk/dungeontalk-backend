f503723eb4064a16e76363aae66af948e2c2fb7e
codesche <codesche@gmail.com>
2025-08-11 10:59:28 +0900

    feat(chat): 퇴장/재입장 상태 보여주기 (PRESENCE)

--- src/main/java/org/com/dungeontalk/domain/chat/common/MessageType.java
+++ src/main/java/org/com/dungeontalk/domain/chat/common/MessageType.java
@@ -1,7 +1,7 @@
 package org.com.dungeontalk.domain.chat.common;
 
 public enum MessageType {
-    JOIN, TALK, LEAVE, CONNECTED_COUNT;
+    JOIN, TALK, LEAVE, CONNECTED_COUNT, PRESENCE;
 
     // @Enumerated 반영 안 되는 이슈 처리
     @Override
