8d2e290d8b6c7d276650afc3ea8f5fb4e279cabb
codesche <codesche@gmail.com>
2025-08-13 17:25:38 +0900

    refactor(chat): String -> ChatMode(enum) 으로 변경

--- src/main/java/org/com/dungeontalk/domain/chat/dto/ChatRoomDto.java
+++ src/main/java/org/com/dungeontalk/domain/chat/dto/ChatRoomDto.java
@@ -5,16 +5,16 @@ import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.com.dungeontalk.domain.chat.common.ChatMode;
 import org.com.dungeontalk.domain.chat.entity.ChatRoom;
 
 @Getter
-@Setter
 @Builder
 public class ChatRoomDto {
 
     private String id;
     private String roomName;
-    private String mode;
+    private ChatMode mode;
     private Instant createdAt;
     private Instant updatedAt;
 
@@ -22,7 +22,7 @@ public class ChatRoomDto {
         return ChatRoomDto.builder()
             .id(room.getId())
             .roomName(room.getRoomName() != null ? room.getRoomName() : "UNKNOWN")
-            .mode(room.getMode().name())
+            .mode(room.getMode())
             .createdAt(room.getCreatedAt())
             .updatedAt(room.getUpdatedAt())
             .build();
